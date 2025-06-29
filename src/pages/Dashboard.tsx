027266648473730405b8fdb4a9208a5047e91018
angaar3456 <angaar3456@gmail.com>
2025-06-29 09:52:16 +0530

    Fix TypeScript build errors

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -129,7 +129,6 @@ export default function Dashboard() {
                 onRecipesLoaded={handleRecipesLoaded}
                 isDetecting={isDetecting}
                 setIsDetecting={setIsDetecting}
-                isLoadingRecipes={isLoadingRecipes}
                 setIsLoadingRecipes={setIsLoadingRecipes}
               />
             )}
